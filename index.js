7b8c48e03b25ae439013609253fc606f63b6d95b
Ramazan Bakır <ramazan.bakir.software@gmail.com>
2020-12-03 23:57:16 +0300

    Error Handling and Callback Example

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+const rectangle = require('./rectangle');
 var rect = require('./rectangle');
 
 function solveRect(l,b)
 {
     console.log("Solving for rectangle with l= "+l+" and b= "+b);
-    if(l<=0 || b<=0)
-    {
-        console.log("Rectangle dimensions should be greater than zero : l= "+l+"and b= "+b);
-    }
-    else
-    {
-        console.log("The area of the rectangle is "+rect.area(l,b));
-        console.log("The perimeter of the rectangle is "+rect.perimeter(l,b));
-    }
+    rect(l,b, (err,rectangle) =>{
+        //In this part is executed by 2 sec. delay Because of the we are using to (set time out funct.) in rectangle.js file
+        if(err)
+        {
+            console.log("Error: ",err.message);
+        }
+        else
+        {
+            console.log("The area of the rectangle of dimensions l= "+l+
+            " and b= "+b+" is "+rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l= "+l+
+            " and b= "+b+" is "+rectangle.perimeter());
+        }
+
+    });
+    console.log("This statement is after the call to rect()");
 }
 
 solveRect(2,4);
